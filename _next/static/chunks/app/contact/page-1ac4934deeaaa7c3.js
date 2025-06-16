0de5cafd471682be3ff5ad4cc5178cdd37b10363
alexsanya <alexsanyakoval@gmail.com>
2025-06-15 19:42:41 -0500

    init

---
+++ _next/static/chunks/app/contact/page-1ac4934deeaaa7c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{6632:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,2256))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,256,441,517,358],()=>s(6632)),_N_E=e.O()}]);
